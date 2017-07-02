92076eb4949393f85a421824acd7fa80f45d93f4
toyjack <toyjack@gmail.com>
2017-07-03 03:53:43 +0900

    prepare for json2csv

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import bootstrap from 'bootstrap';
 import bootstrapTable from 'bootstrap-table';
 import 'jquery-csv';
 import './lib/jquery.selection';
+import json2csv from 'json2csv';
 
 //ほかのラブライブに使われたjQueryのため
 window.jQuery = jQuery;
@@ -19,102 +20,13 @@ $(document).ready(function () {
     $('#save').click(function () {
         let selectedRow = ""
         selectedRow = $('input[name="btSelectItem"]:checked').data('index')
-        var newValue = $('#textarea').val()
+        let newValue = $('#textarea').val()
         $('#table').bootstrapTable('updateCell', {
             index: selectedRow,
             field: 'KR_def',
             value: newValue
         })
     });
-
-    //https://stackoverflow.com/questions/40428850/how-to-export-data-from-table-to-csv-file-using-jquery
-    $('#export').click(function () {
-        var titles = [];
-        var data = [];
-
-        /*
-         * Get the table headers, this will be CSV headers
-         * The count of headers will be CSV string separator
-         */
-        $('#table th').not('.bs-checkbox ').each(function () { //radioを序がする
-            titles.push($(this).text());
-        });
-
-        /*
-         * Get the actual data, this will contain all the data, in 1 array
-         */
-        $('#table td').not('.bs-checkbox ').each(function () {　 //radioを序がする
-            data.push($(this).html());
-        });
-
-        /*
-         * Convert our data to CSV string
-         */
-        var CSVString = prepCSVRow(titles, titles.length, '');
-        CSVString = prepCSVRow(data, titles.length, CSVString);
-
-        /*
-         * Make CSV downloadable
-         */
-        var downloadLink = document.createElement("a");
-        var blob = new Blob(["\ufeff", CSVString]);
-        var url = URL.createObjectURL(blob);
-        downloadLink.href = url;
-        downloadLink.download = "data.csv";
-
-        /*
-         * Actually download CSV
-         */
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-    });
-
-    /*
-     * Convert data array to CSV string
-     * @param arr {Array} - the actual data
-     * @param columnCount {Number} - the amount to split the data into columns
-     * @param initial {String} - initial string to append to CSV string
-     * return {String} - ready CSV string
-     */
-    function prepCSVRow(arr, columnCount, initial) {
-        var row = ''; // this will hold data
-        var delimeter = ','; // data slice separator, in excel it's `;`, in usual CSv it's `,`
-        var newLine = '\r\n'; // newline separator for CSV row
-
-        /*
-         * Convert [1,2,3,4] into [[1,2], [3,4]] while count is 2
-         * @param _arr {Array} - the actual array to split
-         * @param _count {Number} - the amount to split
-         * return {Array} - splitted array
-         */
-        function splitArray(_arr, _count) {
-            var splitted = [];
-            var result = [];
-            _arr.forEach(function (item, idx) {
-                if ((idx + 1) % _count === 0) {
-                    splitted.push(item);
-                    result.push(splitted);
-                    splitted = [];
-                } else {
-                    splitted.push(item);
-                }
-            });
-            return result;
-        }
-        var plainArr = splitArray(arr, columnCount);
-        // don't know how to explain this
-        // you just have to like follow the code
-        // and you understand, it's pretty simple
-        // it converts `['a', 'b', 'c']` to `a,b,c` string
-        plainArr.forEach(function (arrItem) {
-            arrItem.forEach(function (item, idx) {
-                row += item + ((idx + 1) === arrItem.length ? '' : delimeter);
-            });
-            row += newLine;
-        });
-        return initial + row;
-    }
 
     $('#wakun').click(function () {
         $('#textarea')
@@ -160,6 +72,97 @@ $(document).ready(function () {
                 mode: 'after'
             });
     });
+
+    //https://stackoverflow.com/questions/40428850/how-to-export-data-from-table-to-csv-file-using-jquery
+    $('#export').click(function () {
+        let titles = [];
+        let data = [];
+
+        /*
+         * Get the table headers, this will be CSV headers
+         * The count of headers will be CSV string separator
+         */
+        $('#table th').not('.bs-checkbox ').each(function () { //radioを除外する
+            titles.push($(this).text());
+        });
+
+        /*
+         * Get the actual data, this will contain all the data, in 1 array
+         */
+        $('#table td').not('.bs-checkbox ').each(function () {　 //radioを除外する
+            data.push($(this).html());
+        });
+
+        /*
+         * Convert our data to CSV string
+         */
+        let CSVString = prepCSVRow(titles, titles.length, '');
+        CSVString = prepCSVRow(data, titles.length, CSVString);
+
+        /*
+         * Make CSV downloadable
+         */
+        let downloadLink = document.createElement("a");
+        let blob = new Blob(["\ufeff", CSVString]);
+        let url = URL.createObjectURL(blob);
+        downloadLink.href = url;
+        downloadLink.download = "data.csv";
+
+        /*
+         * Actually download CSV
+         */
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    });
+
+    /*
+     * Convert data array to CSV string
+     * @param arr {Array} - the actual data
+     * @param columnCount {Number} - the amount to split the data into columns
+     * @param initial {String} - initial string to append to CSV string
+     * return {String} - ready CSV string
+     */
+    function prepCSVRow(arr, columnCount, initial) {
+        let row = ''; // this will hold data
+        let delimeter = ','; // data slice separator, in excel it's `;`, in usual CSv it's `,`
+        let newLine = '\r\n'; // newline separator for CSV row
+
+        /*
+         * Convert [1,2,3,4] into [[1,2], [3,4]] while count is 2
+         * @param _arr {Array} - the actual array to split
+         * @param _count {Number} - the amount to split
+         * return {Array} - splitted array
+         */
+        function splitArray(_arr, _count) {
+            let splitted = [];
+            let result = [];
+            _arr.forEach(function (item, idx) {
+                if ((idx + 1) % _count === 0) {
+                    splitted.push(item);
+                    result.push(splitted);
+                    splitted = [];
+                } else {
+                    splitted.push(item);
+                }
+            });
+            return result;
+        }
+        let plainArr = splitArray(arr, columnCount);
+        // don't know how to explain this
+        // you just have to like follow the code
+        // and you understand, it's pretty simple
+        // it converts `['a', 'b', 'c']` to `a,b,c` string
+        plainArr.forEach(function (arrItem) {
+            arrItem.forEach(function (item, idx) {
+                row += item + ((idx + 1) === arrItem.length ? '' : delimeter);
+            });
+            row += newLine;
+        });
+        return initial + row;
+    }
+
+    
 });
 // from https://github.com/evanplaice/jquery-csv/blob/master/examples/file-handling.html
 function isAPIAvailable() {
@@ -186,10 +189,10 @@ function isAPIAvailable() {
 }
 
 function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
-    var file = files[0];
+    let files = evt.target.files; // FileList object
+    let file = files[0];
     // read the file metadata
-    var output = ''
+    let output = ''
     output += '<span style="font-weight:bold;">' + escape(file.name) + '</span><br />\n';
     output += ' - FileType: ' + (file.type || 'n/a') + '<br />\n';
     output += ' - FileSize: ' + file.size + ' bytes<br />\n';
@@ -202,17 +205,17 @@ function handleFileSelect(evt) {
 }
 
 function printTable(file) {
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.readAsText(file);
     reader.onload = function (event) {
-        var csv = event.target.result;
-        var data = $.csv.toArrays(csv);
-        var obCSV = $.csv.toObjects(csv);
-        var jsonobject = JSON.stringify(obCSV);
-        // var html = '';
-        // for (var row in data) {
+        let csv = event.target.result;
+        let data = $.csv.toArrays(csv);
+        let obCSV = $.csv.toObjects(csv);
+        let jsonobject = JSON.stringify(obCSV);
+        // let html = '';
+        // for (let row in data) {
         //     html += '<tr>\r\n';
-        //     for (var item in data[row]) {
+        //     for (let item in data[row]) {
         //         html += '<td>' + data[row][item] + '</td>\r\n';
         //     }
         //     html += '</tr>\r\n';
@@ -279,4 +282,13 @@ function printTable(file) {
     reader.onerror = function () {
         alert('Unable to read ' + file.fileName);
     };
+}
+
+function makeCSV(obCSV) {
+    let csvFields = ['KRID', 'KR2ID', 'KR_Tenri_p', 'KR_vol_radical', 'KR_vol_name', 'KR_radical', 'Entry', 'Entry_original', 'KR_def']
+    let csvdata = json2csv({
+        data: obCSV,
+        fields: csvFields
+    })
+    console.log(csvdata)
 }
